3a0f306b619f50afe7af00cde25be9b5aa199f8f
acompt <andreacompton54@gmail.com>
2015-03-07 20:36:44 -0500

    fixed json

--- messages/lab.js
+++ messages/lab.js
@@ -1,20 +1,25 @@
+function parse() {
+	request = new XMLHttpRequest();
+	request.open('GET', 'data.json', true);
+	request.onreadystatechange = parseData();
+	request.send();
+}
 
 function parseData() {
 	if (request.readystate == 4 && request.status == 200) {
-		messagesDiv = document.getElementById(“messages”)
+		messagesDiv = document.getElementById('messages');
 		converted = JSON.parse(request.responseText);
+		for (i = 0; i < converted.length; i++) {
+			messagesDiv.innerHTML += converted[i]["content"];
+		}
 	}
-	for (i = 0; i < converted.length; i++) {
-		messagesDiv.innerHTML += <p> + converted[i][‘content’]
-		if request.readystate == 4 && request.status != 200
-			alert(“hacked”)
-	}
+	if (request.readystate == 4 && request.status != 200)
+				alert('did not load');
 }
 
-function parse() {
-	Request = new XMLHttpRequest();
-	Request.open(“GET”, “messages.json”, true);
-	Request.onreadystatechenge = parseData();
-	Request.send()
-}
+
+
+
+
+
 
