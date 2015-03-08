b8c2190b23166a0aac4a4805b321956f453d2c70
acompt <andreacompton54@gmail.com>
2015-03-07 19:42:12 -0500

    added json code

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,20 @@
-// Your JavaScript goes here...
+
+function parseData() {
+	if (request.readystate == 4 && request.status == 200) {
+		messagesDiv = document.getElementById(“messages”)
+		converted = JSON.parse(request.responseText);
+	}
+	for (i = 0; i < converted.length; i++) {
+		messagesDiv.innerHTML += <p> + converted[i][‘content’]
+		if request.readystate == 4 && request.status != 200
+			alert(“hacked”)
+	}
+}
+
+function parse() {
+	Request = new XMLHttpRequest();
+	Request.open(“GET”, “messages.json”, true);
+	Request.onreadystatechenge = parseData();
+	Request.send()
+}
+
