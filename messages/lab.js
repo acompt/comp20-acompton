22e789c9365f45e91dedaaa62bc21e441555fe35
acompt <andreacompton54@gmail.com>
2015-03-08 18:59:49 -0400

    put inside parse

--- messages/lab.js
+++ messages/lab.js
@@ -4,23 +4,23 @@ function parse() {
 	request.open("GET", "data.json", true);
 	request.onreadystatechange = parseData;
 	request.send();
-}
 
-function parseData() {
-	console.log("in parse data")
-	if (request.readystate == 4 && request.status == 200) {
-		messagesDiv = document.getElementById("messages");
-		converted = JSON.parse(request.responseText);
-		for (i = 0; i < converted.length; i++) {
-			messagesDiv.innerHTML += converted[i]['content'];
-			console.log(messagesDiv.innerHTML);
+
+	function parseData() {
+		console.log("in parse data")
+		if (request.readystate == 4 && request.status == 200) {
+			messagesDiv = document.getElementById("messages");
+			converted = JSON.parse(request.responseText);
+			for (i = 0; i < converted.length; i++) {
+				messagesDiv.innerHTML += converted[i]['content'];
+				console.log(messagesDiv.innerHTML);
+			}
 		}
+		if (request.readystate == 4 && request.status != 200) {
+			alert("did not load");
+		}
+		console.log(request.readystate);
 	}
-	if (request.readystate == 4 && request.status != 200) {
-		alert("did not load");
-	}
-	console.log(request.readystate);
+
 }
-
-
 
