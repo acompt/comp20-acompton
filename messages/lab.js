5c3b76ab4d21e125be5708054c3ce2f9597c2ec7
acompt <andreacompton54@gmail.com>
2015-03-07 20:52:41 -0500

    removed ()

--- messages/lab.js
+++ messages/lab.js
@@ -1,24 +1,23 @@
 
-function parseData() {
-	if (request.readystate == 4 && request.status == 200) {
-		messagesDiv = document.getElementById('messages');
-		converted = JSON.parse(request.responseText);
-		for (i = 0; i < converted.length; i++) {
-			messagesDiv.innerHTML += converted[i]["content"];
-		}
-	}
-	if (request.readystate == 4 && request.status != 200)
-				alert('did not load');
-}
-
-
 function parse() {
 	request = new XMLHttpRequest();
-	request.open('GET', 'data.json', true);
-	request.onreadystatechange = parseData();
+	request.open("get", "data.json", true);
+	request.onreadystatechange = parseData;
 	request.send();
 }
 
+function parseData() {
+	console.log("in parse data")
+	if (request.readystate == 4 && request.status == 200) {
+		messagesDiv = document.getElementById("messages");
+		converted = JSON.parse(request.responseText);
+		for (i = 0; i < converted.length; i++) {
+			messagesDiv.innerHTML += converted[i]['content'];
+		}
+	}
+	if (request.readystate == 4 && request.status != 200)
+		alert("did not load");
+}
 
 
 
