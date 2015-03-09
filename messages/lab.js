e67095995d50d0a3123025e1eef575b24a5247e6
acompt <andreacompton54@gmail.com>
2015-03-08 20:42:05 -0400

    caps error

--- messages/lab.js
+++ messages/lab.js
@@ -5,10 +5,9 @@ function parse() {
 	request.onreadystatechange = parseData;
 	request.send();
 
-
 	function parseData() {
 		console.log("in parse data")
-		if (request.readystate == 4 && request.status == 200) {
+		if (request.readyState == 4 && request.status == 200) {
 			messagesDiv = document.getElementById("messages");
 			converted = JSON.parse(request.responseText);
 			for (i = 0; i < converted.length; i++) {
@@ -16,10 +15,10 @@ function parse() {
 				console.log(messagesDiv.innerHTML);
 			}
 		}
-		if (request.readystate == 4 && request.status != 200) {
+		if (request.readyState == 4 && request.status != 200) {
 			alert("did not load");
 		}
-		console.log(request.readystate);
+		console.log(request.readyState);
 	}
 
 }
