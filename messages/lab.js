86e985e1da62cee5fb454a1b27f02d1df192d4c7
acompt <andreacompton54@gmail.com>
2015-03-09 20:13:53 -0400

    changing back

--- messages/lab.js
+++ messages/lab.js
@@ -1,7 +1,7 @@
 
 function parse() {
 	request = new XMLHttpRequest();
-	request.open("get", "data.json", true);
+	request.open("get", "http://messagehub.herokuapp.com/messages.json", true);
 	request.onreadystatechange = parseData;
 	request.send();
 
